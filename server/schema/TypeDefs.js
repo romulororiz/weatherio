c90b77f64e26050f4b5ec86feafe00ef16ea9cc9
Romulo Roriz <romulonmroriz@gmail.com>
2022-11-21 18:54:23 +0100

    Define Types for weather query

---
+++ server/schema/TypeDefs.js
@@ -0,0 +1,51 @@
+import { gql } from 'graphql-tag';
+
+export const typeDefs = gql`
+	# Queries
+	type Query {
+		CityWeather(name: String, cnt: Int): Weather!
+	}
+
+	# Types
+	type Weather {
+		cnt: Int
+		list: [List!]!
+		city: City!
+	}
+
+	type List {
+		dt: Int!
+		main: WeatherMain!
+		weather: [WeatherType]!
+		wind: Wind!
+		dt_txt: String!
+	}
+
+	type WeatherMain {
+		temp: Float!
+		feels_like: Float!
+		temp_min: Float!
+		temp_max: Float!
+		pressure: Float!
+		humidity: Float!
+	}
+
+	type WeatherType {
+		id: ID!
+		main: String!
+		description: String!
+		icon: String!
+	}
+
+	type Wind {
+		speed: Float!
+	}
+
+	type City {
+		id: ID!
+		name: String!
+		country: String!
+		sunrise: Int!
+		sunset: Int!
+	}
+`;

ec236c6c471b64918b391013b4d7c03a7563e77e
Romulo Roriz <romulonmroriz@gmail.com>
2022-12-05 18:02:43 +0100

    Add purgecss

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,10 +1,16 @@
 const { src, dest, watch, series, task } = require('gulp');
 const autoPrefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
 	return src('./src/styles/scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(
+			purgecss({
+				content: ['./src/**/*.jsx'],
+			})
+		)
 		.pipe(autoPrefixer())
 		.pipe(dest('./src/styles/css'));
 }

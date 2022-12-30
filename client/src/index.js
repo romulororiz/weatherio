5203a8e0627818dc074e84e81b75f2674439a999
Romulo Roriz <romulonmroriz@gmail.com>
2022-12-30 14:48:05 +0100

    Add Context Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HistoryContextProvider } from './context/HistoryContext';
+import './styles/scss/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<HistoryContextProvider>
+			<App />
+		</HistoryContextProvider>
 	</React.StrictMode>
 );

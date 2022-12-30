29c04d5dbd91bf9b4702a7eb570231f2654aea5f
Romulo Roriz <romulonmroriz@gmail.com>
2022-12-30 14:54:01 +0100

    Setup Express Server w/ ApolloServer

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
@@ -6,6 +8,9 @@ import { resolvers } from './schema/Resolvers.js';
 import { typeDefs } from './schema/TypeDefs.js';
 import cors from 'cors';
 import Dotenv from 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 
@@ -22,6 +27,14 @@ await server.start();
 
 app.use(cors(), express.json(), express.urlencoded({ extended: true }));
 app.use('/graphql', expressMiddleware(server));
+
+// Serve frontend
+// Set build folder as static
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+});
 
 // Modified server startup
 await new Promise(resolve => httpServer.listen({ port: PORT }, resolve));

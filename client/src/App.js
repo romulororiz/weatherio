5a84ffa881dd15dc9a6158a6a2fd3e1cb6339603
Romulo Roriz <romulonmroriz@gmail.com>
2022-12-30 14:47:49 +0100

    Define routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import { GlobalStyle } from './styles';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 import { ApolloProvider } from '@apollo/client';
 import { apolloClient } from './graphql/apolloClient';
+import Dashboard from './components/Dashboard';
+import NotFound from './pages/NotFound';
 
 function App() {
 	return (
 		<ApolloProvider client={apolloClient}>
-			<GlobalStyle />
 			<Router>
 				<Routes>
-					{/* <Route path='/' exact element={<Home />} /> */}
+					<Route path='/' exact element={<Dashboard />} />
+					<Route path='*' exact element={<NotFound />} />
 				</Routes>
 			</Router>
 		</ApolloProvider>
